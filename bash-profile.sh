5b0c1a922ef37c41a87e056dd46f167e94ad4c8d
Michael Barrett <twisterghost@gmail.com>
2014-10-01 17:26:03 -0400

    Fix up bash profile indentation
    Also remove useless function

--- bash-profile.sh
+++ bash-profile.sh
@@ -1,91 +1,61 @@
+# Import the git branch completion script if its there.
 if [ -f ~/scripts/.git-completion.bash ]; then
   . ~/scripts/.git-completion.bash
 fi
 
 
-# return the current branch name
+# Returns the current branch name.
 function parse_current_branch {
   git symbolic-ref --short HEAD
 }
 
-function proml {
-    local      YELLOW="\[\033[1;33m\]"
-    local        BLUE="\[\033[0;34m\]"
-    local  LIGHT_BLUE="\[\033[1;34m\]"
-    local        CYAN="\[\033[0;36m\]"
-    local  LIGHT_CYAN="\[\033[1;36m\]"
-    local         RED="\[\033[0;31m\]"
-    local   LIGHT_RED="\[\033[1;31m\]"
-    local       GREEN="\[\033[0;32m\]"
-    local LIGHT_GREEN="\[\033[1;32m\]"
-    local       WHITE="\[\033[1;37m\]"
-    local  LIGHT_GRAY="\[\033[0;37m\]"
-    local    EC="\[\e[m\]" #end color
-    case $TERM in
-        xterm*)
-        TITLEBAR='\[\033]0;\u@\h:\w\007\]'
-        ;;
-        *)
-        TITLEBAR=""
-        ;;
-    esac
+function customPrompt {
 
-PS1="${TITLEBAR}\
-$LIGHT_BLUE\e dev[$EC$LIGHT_RED$A\w$EC$LIGHT_GREEN|\$(parse_current_branch)$EC$LIGHT_BLUE]$EC\
-$LIGHT_BLUE\$$EC "
-PS2='> '
-PS4='+ '
-}
+  EXITSTATUS="$?"
+  BOLD="\[\033[1m\]"
+  RED="\[\033[1;31m\]"
+  GREEN="\[\e[32;1m\]"
+  BLUE="\[\e[34;1m\]"
+  OFF="\[\033[m\]"
 
-#proml
+  PROMPT="[\t] #\W"
 
-function exitstatus {
+  if [ "${EXITSTATUS}" -eq 0 ]
+  then
+     PS1="${PROMPT}${BOLD}${GREEN}:${OFF} "
+  else
+     PS1="${PROMPT}${BOLD}${RED}:${OFF} "
+  fi
 
-    EXITSTATUS="$?"
-    BOLD="\[\033[1m\]"
-    RED="\[\033[1;31m\]"
-    GREEN="\[\e[32;1m\]"
-    BLUE="\[\e[34;1m\]"
-    OFF="\[\033[m\]"
-
-    PROMPT="[\t] #\W"
-
-    if [ "${EXITSTATUS}" -eq 0 ]
-    then
-       PS1="${PROMPT}${BOLD}${GREEN}:${OFF} "
-    else
-       PS1="${PROMPT}${BOLD}${RED}:${OFF} "
-    fi
-
-    PS2="${BOLD}and then... ${OFF} "
+  PS2="${BOLD}and then... ${OFF} "
 }
 
 PROMPT_COMMAND=exitstatus
 
 function gpo {
-    git push origin $1
+  git push origin $1
 }
 
 function gcm {
-    git commit -m "$1"
+  git commit -m "$1"
 }
 
 newbranch() {
-    git stash
-    git fetch
-    git checkout master
-    git pull origin master
-    git checkout -b $1
+  git stash
+  git fetch
+  git checkout master
+  git pull origin master
+  git checkout -b $1
 }
 
 # Shortcut for pulling the current branch
 pull() {
-    git pull origin $(parse_current_branch)
+  git pull origin $(parse_current_branch)
 }
 
 # Shortcut for pushing the current branch
 push() {
-    git push origin $(parse_current_branch)
+  git push origin $(parse_current_branch)
 }
 
 # merge master into your current branch
@@ -94,11 +64,11 @@ push() {
 #
 alias mim="mergeInMaster"
 mergeInMaster() {
-    git checkout master
-    git fetch
-    git pull origin master
-    git checkout @{-1}
-    git merge master
+  git checkout master
+  git fetch
+  git pull origin master
+  git checkout @{-1}
+  git merge master
 }
 
 # obtain a clean version of a branch from remote
@@ -106,11 +76,11 @@ mergeInMaster() {
 # $ cleanbranch qa
 #
 cleanbranch() {
-    git checkout master
-    git branch -D $1
-    git fetch
-    git checkout $1
-    git pull origin $1
+  git checkout master
+  git branch -D $1
+  git fetch
+  git checkout $1
+  git pull origin $1
 }
 
 if [ -f ~/.git-completion.bash ]; then
