42a54cf31e6faf5f6943e956eed451daec0d4375
Michael Barrett <twisterghost@gmail.com>
2015-12-16 13:23:24 -0500

    Add 'attach' alias

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -10,6 +10,9 @@ alias hosts="$SOFT_EDITOR /etc/hosts"
 
 
 ## Mini scripts
+function attach {
+  tmux attach -t $1
+}
 
 # Strip all trailing whitespace from files, recursively.
 alias striptrailspace="ack --print0 -l '[ \t]+$' | xargs -0 -n1 perl -pi -e 's/[ \t]+$//'"
