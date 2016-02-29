b94b73b5fc49f77f7538cd2fc7d3fa6fd64989c9
Michael Barrett <twisterghost@gmail.com>
2016-02-29 10:38:03 -0500

    Delete old files and functions

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -2,10 +2,7 @@
 
 # Navigation & tools
 alias ..="cd .."
-alias cls="clear;pwd;ls"
 alias resource="source ~/.bash_profile"
-alias edit-config="$SOFT_EDITOR ~/my-configs/bash-profile.sh"
-alias edit-profile="$SOFT_EDITOR ~/.bash_profile"
 alias hosts="$SOFT_EDITOR /etc/hosts"
 
 
@@ -16,4 +13,3 @@ function attach {
 
 # Strip all trailing whitespace from files, recursively.
 alias striptrailspace="ack --print0 -l '[ \t]+$' | xargs -0 -n1 perl -pi -e 's/[ \t]+$//'"
-alias cleanswaps="rm -i \`find . | grep .swp$\`"
--- scripts/git-functions.sh
+++ scripts/git-functions.sh
@@ -12,25 +12,6 @@ function parse-current-branch {
   #git symbolic-ref --short HEAD
 }
 
-# Shortening for 'git push origin XYZ'
-function gpo {
-  git push origin $1
-}
-
-# Shortening for 'git commit -m XYZ'
-function gcm {
-  git commit -m "$1"
-}
-
-# Creates a new branch based off of master.
-function new-branch() {
-  git stash
-  git fetch
-  git checkout master
-  git pull origin master
-  git checkout -b $1
-}
-
 # Shortcut for pulling the current branch
 function pull() {
   git pull origin $(parse-current-branch)
@@ -41,28 +22,8 @@ function push() {
   git push origin $(parse-current-branch)
 }
 
-# Merges master into your current branch.
-alias mim="merge-in-master"
-function merge-in-master() {
-  git checkout master
-  git fetch
-  git pull origin master
-  git checkout @{-1}
-  git merge master
-}
-
-# Obtain a clean version of a branch from remote.
-function clean-branch() {
-  git checkout master
-  git branch -D $1
-  git fetch
-  git checkout $1
-  git pull origin $1
-}
-
 # Git related aliases, mostly shortenings.
 alias gaa="git add -A"
 alias gs="git status"
 alias forgit="git reset --hard"
-alias refresh="git stash;git checkout master;git fetch;git pull --rebase origin master;"
 alias undocommit="git reset --soft 'HEAD^'"
