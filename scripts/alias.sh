cb735339f43062a833e80c4070b17798f180474e
Michael Barrett <twisterghost@gmail.com>
2016-08-15 12:43:42 -0400

    Update alias.sh

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -6,10 +6,9 @@ alias resource="source ~/.bash_profile"
 alias hosts="$SOFT_EDITOR /etc/hosts"
 
 
-## Mini scripts
+# FUNCTIONS
+
+# Shortcut tmux attach
 function attach {
   tmux attach -t $1
 }
-
-# Strip all trailing whitespace from files, recursively.
-alias striptrailspace="ack --print0 -l '[ \t]+$' | xargs -0 -n1 perl -pi -e 's/[ \t]+$//'"
