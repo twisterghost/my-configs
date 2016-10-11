8e1dd549001973d864c4c8b71d095dac371fa167
Michael Barrett <twisterghost@gmail.com>
2016-10-11 11:56:26 -0400

    Review vim plugin list

--- nvim/installExtensions.sh
+++ nvim/installExtensions.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 # Clone my preferred vim addons
 
+echo "Creating bundle directory...
 mkdir -p bundle
 cd bundle
-git clone https://github.com/statianzo/vim-jade.git
-git clone https://github.com/vim-scripts/TwitVim.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/ap/vim-buftabline.git
-git clone https://github.com/pangloss/vim-javascript.git
-git clone https://github.com/rking/ag.vim.git
-git clone https://github.com/vim-airline/vim-airline.git
-git clone https://github.com/vim-airline/vim-airline-themes.git
-git clone https://github.com/rhysd/vim-crystal.git
-git clone https://github.com/mxw/vim-jsx.git
-git clone https://github.com/kien/ctrlp.vim.git
-git clone https://github.com/groenewege/vim-less.git
-git clone https://github.com/ntpeters/vim-better-whitespace.git
-git clone https://github.com/jonathanfilip/vim-lucius.git
+
+# Utilities
+echo "Cloning utilities..."
+git clone -q https://github.com/scrooloose/nerdtree.git
+git clone -q https://github.com/ap/vim-buftabline.git
+git clone -q https://github.com/rking/ag.vim.git
+git clone -q https://github.com/vim-airline/vim-airline.git
+git clone -q https://github.com/vim-airline/vim-airline-themes.git
+git clone -q https://github.com/kien/ctrlp.vim.git
+git clone -q https://github.com/ntpeters/vim-better-whitespace.git
+
+# Languages
+echo "Cloning language syntaxes..."
+git clone -q https://github.com/pangloss/vim-javascript.git
+git clone -q https://github.com/mxw/vim-jsx.git
+git clone -q https://github.com/rhysd/vim-crystal.git
+
+# Color schemes
+echo "Cloning color schemes..."
+git clone -q https://github.com/jonathanfilip/vim-lucius.git
+
+echo "Done."
 cd ..
