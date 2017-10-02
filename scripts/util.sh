a17b2a07b017f1a2ec2fa69124a1f39578b7fc78
Michael Barrett <twisterghost@users.noreply.github.com>
2017-10-01 21:30:26 -0400

    Put utils in the utils file
    How novel

--- scripts/util.sh
+++ scripts/util.sh
@@ -1,5 +1,3 @@
-# Create a symlink to a config file in my-configs.
-
 function link-config() {
   if [ "$#" -ne 2 ]
   then
@@ -7,4 +5,19 @@ function link-config() {
     return 0
   fi
   ln -s ~/my-configs/$2 $1
+}
+
+# Shortcut tmux attach
+function attach {
+  tmux attach -t $1
+}
+
+# Shortcut tmux new -s
+function ses {
+  tmux new -s $1
+}
+
+# Kill a process on the given port
+function kill-port() {
+  kill -9 `sudo lsof -t -i:$1`
 }
