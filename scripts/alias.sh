dd761958bdb5d6b64a8c194be4236bf5a5783de6
Michael Barrett <twisterghost@users.noreply.github.com>
2017-10-01 21:30:07 -0400

    Actually, thats dumb, these are utils

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -2,18 +2,3 @@
 alias ..="cd .."
 alias resource="source ~/.bash_profile"
 alias hosts="$SOFT_EDITOR /etc/hosts"
-
-# Shortcut tmux attach
-function attach {
-  tmux attach -t $1
-}
-
-# Shortcut tmux new -s
-function ses {
-  tmux new -s $1
-}
-
-# Kill a process on the given port
-function kill-port() {
-  kill -9 `sudo lsof -t -i:$1`
-}
