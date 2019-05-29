d0ba257b14bf26e030bd7bf6c03822f92390b14a
Michael Barrett <twisterghost@users.noreply.github.com>
2019-05-29 16:52:31 -0400

    Add goyo

--- nvim/installExtensions.sh
+++ nvim/installExtensions.sh
@@ -15,6 +15,7 @@ git clone -q https://github.com/kien/ctrlp.vim.git
 git clone -q https://github.com/ntpeters/vim-better-whitespace.git
 git clone -q https://github.com/jremmen/vim-ripgrep.git
 git clone -q git://github.com/tpope/vim-endwise.git
+git clone -q https://github.com/junegunn/goyo.vim.git
 
 # Languages
 echo "Cloning language syntaxes..."
